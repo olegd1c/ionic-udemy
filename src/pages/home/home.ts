d21659e3eb09137ea65d44c7d1f232fb96384b3f
dikhtyaruk <olegd1c@gmail.com>
2018-06-26 16:02:23 +0300

    vitrual scroll

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public users = [];
+  private page = 0;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+    this.loadUsers();
+    //this.loadUsersVirtual();
   }
 
+  loadUsers(infiniteScroll?) {
+    this.http.get('https://randomuser.me/api/?results=20&page=${this.page}')
+      .subscribe(res => {
+        this.users = this.users.concat(res['results']);
+
+        if(infiniteScroll) {
+          infiniteScroll.complete();
+        }
+      });
+  }
+
+  loadMore(infiniteScroll) {
+    this.page++;
+    this.loadUsers(infiniteScroll);
+  }
+
+  loadUsersVirtual(infiniteScroll?) {
+    this.http.get('https://randomuser.me/api/?results=200')
+      .subscribe(res => {
+        this.users = res['results'];
+      });
+  }
 }
