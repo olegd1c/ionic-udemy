a87ce67dd4e4122574b9a2bc6c614d0a1ca21245
dikhtyaruk <olegd1c@gmail.com>
2018-07-04 16:49:54 +0300

    recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { EditRecipePage } from "../pages/edit-recipe/edit-recipe";
+import { RecipePage } from "../pages/recipe/recipe";
+import { RecipesPage } from "../pages/recipes/recipes";
+import { ShoppingListPage } from "../pages/shopping-list/shopping-list";
+import { TabsPage } from "../pages/tabs/tabs";
+import { ShoppingListService } from "../services/shopping-list";
+import { RecipesService } from "../services/recipes";
+import { SigninPage } from "../pages/signin/signin";
+import { SignupPage } from "../pages/signup/signup";
+import { AuthService } from "../services/auth";
+import { DatabaseOptionsPage } from "../pages/database-options/database-options";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    EditRecipePage,
+    RecipePage,
+    RecipesPage,
+    ShoppingListPage,
+    TabsPage,
+    SigninPage,
+    SignupPage,
+    DatabaseOptionsPage
   ],
   imports: [
-    BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    EditRecipePage,
+    RecipePage,
+    RecipesPage,
+    ShoppingListPage,
+    TabsPage,
+    SigninPage,
+    SignupPage,
+    DatabaseOptionsPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ShoppingListService,
+    RecipesService,
+    AuthService
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
